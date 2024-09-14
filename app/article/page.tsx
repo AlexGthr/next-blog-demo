c02725d6263162ba757a62528410356eae2ad5b5
AlexGthr <agauthier5@outlook.fr>
2024-09-14 17:03:32 +0200

    intégration de la BDD Ajout des collections Affichages des articles sur la page article

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,9 +1,29 @@
+import { db } from '@/lib/db'
 import React from 'react'
 
-const page = () => {
+const ArticlePage = async () => {
+
+  // Récupérer la liste des articles
+  const articles =  await db.article.findMany({
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
+
   return (
-    <div>page</div>
+    <div>
+        <h1 className='text-2xl font-bold uppercase m-5'>Titre du blog</h1>
+        
+        {/* Liste des articles */}
+        {articles.map((article: any) => (
+            <div>
+              <h2 className='font-bold text-center'>{article.title}</h2>
+              <p className='italic m-4'>{article.text}</p>
+              <hr className='w-40 border-1 border-emerald-600 m-auto pb-8'/>
+            </div>
+          ))}
+    </div>
   )
 }
 
-export default page
+export default ArticlePage
